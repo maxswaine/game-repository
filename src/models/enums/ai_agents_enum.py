caf48a8c64b3eb1187a2f3dfdd95a4b14a8b5d55
maxswaine <maxswaine@gmail.com>
2026-04-12 20:05:35 +0100

    as said in branch name

--- src/models/enums/ai_agents_enum.py
+++ src/models/enums/ai_agents_enum.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class AIAgentEnum(str, Enum):
+    description = "description"
     objective = "objective"
     setup = "setup"
     rules = "rules"
--- src/models/game_models/game_vote.py
+++ src/models/game_models/game_vote.py
@@ -3,4 +3,3 @@ from pydantic import BaseModel
 class GameVoteRead(BaseModel):
     game_id: str
     upvotes: int
-    downvotes: int
